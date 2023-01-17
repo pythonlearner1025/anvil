bc4dbdd8894ef71c08bd695ce606c914fcbb23e5
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 14:53:32 -0500

    n

--- backend/down.py
+++ backend/down.py
@@ -20,12 +20,18 @@ if __name__ == '__main__':
     with open('scraped.json', 'r') as f:
         a = json.load(f)
         objs = []
+        cats = dict() 
         for cat in a.keys():
-            cat = a[cat]
-            for subcat in cat.keys():
-                for obj in cat[subcat]:
+            cats[cat] = []
+            cat_dict = a[cat]
+            for subcat in cat_dict.keys():
+                cats[cat].append(subcat)
+                for obj in cat_dict[subcat]:
                     objs.append(obj)
     
+    with open('cats.json', 'w') as f:
+        json.dump(cats, f)
+    exit() 
     for i in tqdm(range(len(objs))):
         down(objs[i])
 
